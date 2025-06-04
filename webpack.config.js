07b9753d368c12dfa75b0b2a7c377a10998edf4f
Miles Coleman <miles.lcoleman@gmail.com>
2025-06-04 09:05:56 -0600

    Added React and other styling changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -13,13 +14,22 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './index.html',
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                from: path.resolve(__dirname, 'static'),
+                to: path.resolve(__dirname, 'dist/static') // or just 'static' if relative to dist
+                }
+            ]
+        })
     ],
     resolve: {
-        extensions: ['.ts', '.js'], // allow .ts imports
+        extensions: ['.ts', '.js', '.jsx'], // allow .ts imports
     },
     devServer: {
         static: './dist',
         open: true,
+        hot: true
     },
     module: {
         rules: [
@@ -32,6 +42,13 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.(js|jsx)$/,   // handle .js and .jsx files
+                exclude: /node_modules/,
+                use: {
+                loader: 'babel-loader'
+                }
+            }
         ],
     },
 };  
